bd3bcc834f8c4d9c8ea01e3b3d5afc7dada3c899
yuval weiss <yuvalweiss1403@gmail.com>
2023-01-30 11:51:37 +0200

    final project

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// get all relevant elements from document
 const shortenItButton = document.getElementById('shorten-it-button');
 const shortenInput = document.getElementById('input');
 const inputEmptyLabel=document.getElementById('input-empty-label');
@@ -6,12 +7,15 @@ shortedContainer.className ='shortedContainer';
 const mobileMenuButton = document.getElementById('mobile-menu');
 const mobileMenu = document.getElementById('mobile-navigation-container');
 
+// create a local storage array for the shorted url elements
 let shortedUrls = JSON.parse(localStorage.getItem("shortedUrls")) || [];
+// render the shorted urls on load
 document.querySelector("body").onload = renderShortedUrls();
 
-
+// fetch the short url from the api if the input is not empty
 shortenItButton.addEventListener('click',()=>{
     const inputText =shortenInput.value;
+    // error if the input is empty
     if(inputText===''){
         shortenInput.style.border='3px solid red';
         inputEmptyLabel.style.display="block";
@@ -27,13 +31,14 @@ shortenItButton.addEventListener('click',()=>{
                 throw new Error(response.statusText);
             }})
         .then((data) => {
-            addTask(inputText,data.result["short_link"]);
+            addShortUrl(inputText,data.result["short_link"]);
         })
         .catch((error) => {
           console.error(error)
         });}
 });
 
+// create a dic for a specific url with its shorted url and a copy button
 function createNewShortedLinkDiv(shortedUrl){
     const container = document.createElement('div');
     const longURL = document.createElement('div');
@@ -54,7 +59,6 @@ function createNewShortedLinkDiv(shortedUrl){
     container.appendChild(longURL);
     container.appendChild(hr);
     container.appendChild(shortURLandCopyContainer);
-    // shortedContainer.appendChild(container);
     copyButton.addEventListener('click',(event)=>{
         copyButton.textContent = 'Copied!';
         copyButton.style.backgroundColor = 'hsl(260, 8%, 14%)';
@@ -63,6 +67,7 @@ function createNewShortedLinkDiv(shortedUrl){
     return container;
 }
 
+// open the menu button on mobile view
 mobileMenuButton.addEventListener('click',()=>{
     let menuDisplay = window.getComputedStyle(mobileMenu).display;
     if(menuDisplay === 'none'){
@@ -72,6 +77,7 @@ mobileMenuButton.addEventListener('click',()=>{
     }
 });
 
+// render the shorted urls from the local storage
 function renderShortedUrls() {
     shortedContainer.innerHTML = "";
     for (let i = 0; i < shortedUrls.length; i++) {
@@ -79,7 +85,9 @@ function renderShortedUrls() {
     }
   }
   
-function addTask(shortURL,longURL) {
+
+// add a new value of shorted url to local storage
+function addShortUrl(shortURL,longURL) {
     const shortedUrl = {
         longUrl: longURL,
         shortUrl:shortURL
